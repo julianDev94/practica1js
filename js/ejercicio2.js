402566c322e3b28eddec3b2f1b1957916a520c6c
JulianAballay <juliancabj1210@gmail.com>
2023-10-26 14:59:47 -0300

    Agrege logica js del ejercicio 2

---
+++ js/ejercicio2.js
@@ -0,0 +1,6 @@
+/* Ejercicio 2: Concatenación de Cadenas */
+
+let nombre = prompt("Ingrese su nombre: ");
+let apellido = prompt("Ingrese su apellido: ");
+let nombreCompleto = nombre + " " + apellido;
+console.log("Nombre completo: " + nombreCompleto);
