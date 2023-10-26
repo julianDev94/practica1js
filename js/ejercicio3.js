a9ed2a3e685aaeb550c2db32c0f2757637d3c076
JulianAballay <juliancabj1210@gmail.com>
2023-10-26 20:36:15 -0300

    Agregue logica de js para el ejercicio 3

---
+++ js/ejercicio3.js
@@ -0,0 +1,6 @@
+/* Ejercicio 3: Conversión de Grados Celsius a Fahrenheit */
+let gradoCelsius = 0;
+
+gradoCelsius = parseInt(prompt("Ingrese los grados celsius: "));
+let gradosFahrenheit = ((gradoCelsius * (9/5)) + 32);
+console.log("Grados Fahrenheit: " + gradosFahrenheit);
