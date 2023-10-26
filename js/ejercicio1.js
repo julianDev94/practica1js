336ab8670b0708e3de12f7cdf369ea4346865ddc
JulianAballay <juliancabj1210@gmail.com>
2023-10-26 14:54:57 -0300

    Agregue logica de js para el ejercicio 1

---
+++ js/ejercicio1.js
@@ -0,0 +1,29 @@
+/*Ejercicio 1: Variables y Operaciones Matemáticas*/
+
+let numero1 = 0;
+let numero2 = 0;
+
+/* EJERCICIO DE SUMA */
+
+numero1 = parseInt(prompt("Ingrese el primer valor: "));
+numero2 = parseInt(prompt("Ingrese el segundo valor: "));
+
+let suma = numero1 + numero2;
+console.log("El resultado de la suma es: " + suma);
+
+/* EJERCICIO DE RESTA */
+
+let resta = numero1 - numero2;
+console.log("El resultado de la resta es: " + resta);
+
+/* EJERCICIO DE MULTIPLICACION */
+
+let multiplicacion = numero1 * numero2;
+console.log("El resultado de la multiplicacion es: " + multiplicacion);
+
+/* EJERCICIO DE DIVISION */
+
+let division = numero1 / numero2;
+
+console.log("El resultado de la division es: " + division);
+
