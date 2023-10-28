90f55d5f9c6832d4f21cd65449da96b38646ccca
JulianAballay <juliancabj1210@gmail.com>
2023-10-27 23:23:06 -0300

    Agregue logica de ejercicio 5 TP - suma de numeros y mostrar por pantalla en tres lineas

---
+++ js/ejercicio5TP.js
@@ -0,0 +1,3 @@
+let numero1 = parseInt(prompt("Ingrese el primer valor: "));
+let numero2 = parseInt(prompt("Ingrese el segundo valor: "));
+document.write(`El resultado de dicha suma es: ${numero1 + numero2}`);
