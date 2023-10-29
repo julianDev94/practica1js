e10a9969a214df56c1f57ea53735c24d0c73ba46
JulianAballay <juliancabj1210@gmail.com>
2023-10-29 13:17:53 -0300

    Agregue logica del ejercicio 9 TP - FALTA SEGUIR RESOLVIENDO

---
+++ js/ejercicio9TP.js
@@ -0,0 +1,8 @@
+let frase = prompt("Ingrese una frase: ");
+let vocales;
+let j = 0;
+for(let i = 0; i < frase.length; i++){
+    if(frase.charAt(i) === 'a' || frase.charAt(i) === 'e' || frase.charAt(i) === 'i' || frase.charAt(i) === 'o' || frase.charAt(i) === 'u' ){
+        
+    }
+}
